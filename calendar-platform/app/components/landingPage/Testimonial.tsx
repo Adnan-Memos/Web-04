0a8ae750d1985fbdbffe920e7ada61fb5c06740f
Adnan Siddiqui <adn.memos@gmail.com>
2025-01-14 02:23:00 +0300

    Implement authentication and dashboard features (#1)
    * Initialized Project, implemented authentication somewhat
    
    * authentication done and dashaboard started
    
    * Updated Theme, Navbar, Dashboard & partially completed Onboarding
    
    * Added Zod and Conform to Typescript, with updates to Onboarding. Error within Onboarding is incomplete
    
    * Checking unique username has an error, until then it's done
    
    * Connection with Nylas began, Onboarding error still persists
    
    * connected to nylas but error with onboarding is still preent
    
    * dashboard basic page is made and the error is resolved
    
    * required components are added from shadcn
    
    * booking page is created
    
    * time slots are added
    
    * calendar componests are created
    
    * availability page in dashboard is done
    
    * event page is done with the delete event feature too
    
    * meetings is done
    
    * settings page is done
    
    * everything is almost done, few changes need to be done
    
    * website is made but the brand logo has to be added
    
    * Update layout.tsx
    
    * logo is added but the booking form has some errors
    
    * Refactor logo imports, update authentication providers, and enhance feature descriptions
    
    ---------
    
    Co-authored-by: sanjanamasam1210 <sanjanamasam1210@gmail.com>

---
+++ calendar-platform/app/components/landingPage/Testimonial.tsx
@@ -0,0 +1,21 @@
+import { SVGProps } from "react";
+
+export function Testimonial() {
+  return (
+    <>
+      <div className="relative max-w-[85rem] px-4 py-10 sm:px-6 lg:px-8 lg:py-14 mx-auto">
+        {/* Blockquote */}
+        <blockquote className="text-center lg:mx-auto lg:w-3/5">
+          <footer className="mt-6">
+            <div className="font-semibold ">Adnan Memos</div>
+            <div className="text-sm text-muted-foreground">
+            Developer | Designer | Photographer
+            </div>
+          </footer>
+        </blockquote>
+        {/* End Blockquote */}
+      </div>
+    </>
+  );
+}
+
